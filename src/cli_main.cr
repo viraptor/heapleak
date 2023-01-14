705632e9c863bb70b28d7748fddf579329e04cad
Stanisław Pitucha <git@viraptor.info>
2023-01-14 16:29:30 +1100

    Initial, works

---
+++ src/cli_main.cr
@@ -0,0 +1,57 @@
+require "./app"
+require "colorize"
+
+action = ARGV.fetch(0, "help")
+
+if action == "stats"
+  if ARGV.size < 4
+    puts("Error: 3 arguments with heap files required")
+    exit
+  end
+  h1, h2, h3 = [load_heap(ARGV[1]), load_heap(ARGV[2]), load_heap(ARGV[3])]
+  leaked_objects = leaked_objects(h1, h2, h3)
+
+  count_class = {} of String => Int32
+  samples = {} of String => Array(HeapLeak::HEAP_ADDRESS)
+  leaked_objects.objects.each do |addr, obj|
+    name = leaked_objects.type_of(addr)
+    count_class[name] = count_class.fetch(name, 0) + 1
+    samples[name] = [] of HeapLeak::HEAP_ADDRESS unless samples.has_key?(name)
+    samples[name].push(addr) unless samples[name].size > 5
+  end
+
+  puts "Potentially leaked objects:".colorize(:light_gray)
+  count_class.each do |k, v|
+    puts "#{k.to_s.colorize(:default)}: #{v.to_s.colorize(:light_blue)}"
+    puts "  #{samples[k].map{|x| "0x#{x.to_s(16)}"}.join(" ").colorize(:dark_gray)}"
+  end
+elsif action == "root"
+  if ARGV.size < 4
+    puts("Error: At least the heap and 2 sample addresses are required to find a common reference")
+    exit
+  end
+  heap = load_heap(ARGV[1])
+  sample_addresses = ARGV[2..].map {|x| x.to_i64(prefix: true)}
+
+  visible_objects = heap.mark_distance_and_visibility(sample_addresses)
+  print_dot_diagram(heap, visible_objects, sample_addresses, max_distance=3)
+else
+  puts "Usage: #{PROGRAM_NAME} ACTION [ARGUMENTS]"
+  puts ""
+  puts "Process Ruby heap dumps created with ObjectSpace.dump_all to get a debug"
+  puts "memory leaks."
+  puts "Both plain and gzipped heaps are supported."
+  puts ""
+  puts "Actions:"
+  puts ""
+  puts "  stats (initial_heap) (first_change_heap) (second_change_heap)"
+  puts "    Get a list of objects allocated in the first change and not freed"
+  puts "    in the second."
+  puts ""
+  puts "  root (first_change_heap) (sample_address1) (sample_address2) ..."
+  puts "    Find a common object which holds the reference to all the provided"
+  puts "    sample addresses (direct or indirect). Print out a .dot diagram"
+  puts "    showing the paths."
+  puts "    Convert it to other formats with (for example):"
+  puts "    dot -Tpng -o foo.png foo.dot"
+end
---
+++ src/heap_process.cr
@@ -0,0 +1,155 @@
+require "json"
+
+module HeapLeak
+  alias HEAP_ADDRESS = Int64
+
+  RUBY_TYPENAMES = {} of String => String
+
+  MAX_DIST = 1000000
+
+  class ShortHeapObject
+    property distance : Int32 = MAX_DIST
+    property seen_from : Set(HEAP_ADDRESS)?
+    property refs : Array(HEAP_ADDRESS)
+    property class_addr : HEAP_ADDRESS?
+    property class_name : String
+    property file : String?
+    property line : String?
+    def initialize(@class_name : String, @class_addr : (HEAP_ADDRESS | Nil), @refs : Array(HEAP_ADDRESS))
+    end
+  end
+
+  class HeapObject
+    include JSON::Serializable
+
+    property address : String?
+    property type : String?
+    @[JSON::Field(key: "class")]
+    property klass : String?
+    property references : Array(String)?
+    property name : String?
+  end
+
+  class Heap
+    getter objects = {} of HEAP_ADDRESS => ShortHeapObject
+    getter back_refs = {} of HEAP_ADDRESS => Array(HEAP_ADDRESS)
+    getter types = {} of HEAP_ADDRESS => String
+
+    def initialize(io : IO)
+      load(io)
+    end
+
+    def initialize(other_heap : Heap)
+      @objects = other_heap.objects.dup
+      @types = other_heap.types.dup
+    end
+
+    def load(io)
+      io.each_line do |line|
+        begin
+          obj = HeapObject.from_json(line)
+        rescue
+          next
+        end
+        next if obj.address.nil?
+
+        if !(o_type = obj.type).nil?
+          RUBY_TYPENAMES[o_type] = o_type unless RUBY_TYPENAMES.has_key?(o_type)
+        end
+
+        short_obj = ShortHeapObject.new(
+          RUBY_TYPENAMES[obj.type],
+          ((klass=obj.klass).nil? ? nil : klass.to_i64(prefix: true)),
+          (obj.references || ([] of String)).map {|x| x.to_i64(prefix: true)}
+        )
+        addr = obj.address.as(String).to_i64(prefix: true)
+        if obj.type == "CLASS" && !(name=obj.name).nil?
+          types[addr] = name
+        end
+
+        objects[addr] = short_obj
+        fill_backref(addr, short_obj)
+      end
+    end
+
+    def fill_backref(addr, short_obj)
+      short_obj.refs.each do |ref|
+        back_refs[ref] = back_refs.fetch(ref, [] of HEAP_ADDRESS).push(addr)
+      end
+    end
+
+    def type_of(addr : HEAP_ADDRESS)
+      obj = objects[addr]
+      if obj.class_name == "CLASS"
+        "CLASS:#{types.fetch(addr, "<unknown>")}"
+      elsif obj.class_name == "OBJECT"
+        types.fetch(obj.class_addr, "<unknown>")
+      else
+        obj.class_name
+      end
+    end
+
+    def mark_distance_and_visibility(sample_addresses : Array(HEAP_ADDRESS), distance_limit = 10)
+      sample_addresses.each do |x|
+        objects[x].distance = 0
+        objects[x].seen_from = Set{x}
+      end
+
+      to_process = sample_addresses.clone
+      seen = Set(HEAP_ADDRESS).new
+
+      max_distance = 0
+
+      while !to_process.empty?
+        current = to_process.pop
+        next_distance = objects[current].distance + 1
+        max_distance = next_distance if next_distance > max_distance
+        seen << current
+        next if next_distance > distance_limit
+        back_refs.fetch(current, [] of HEAP_ADDRESS).each { |x|
+          objects[x].distance = next_distance if (objects[x].distance || MAX_DIST) > next_distance
+          objects[x].seen_from = (objects[x].seen_from || Set(HEAP_ADDRESS).new) + objects[current].seen_from.as(Set(HEAP_ADDRESS))
+          to_process.push(x) unless seen.includes?(x)
+        }
+      end
+
+      seen
+    end
+
+    def common_objects(max_lowest = 3)
+      lowest = MAX_DIST
+      seen.each { |x|
+        if objects[x].seen_from.as(Set(Int64)).size > 1
+          lowest = objects[x].distance if lowest > objects[x].distance
+        end
+      }
+      if lowest < MAX_DIST
+        lowest = max_lowest
+      end
+      seen.each { |x|
+        seen.delete(x) if objects[x].distance > (lowest+2)
+      }
+      seen
+    end
+
+    def remove_objects!(other_heap)
+      other_heap.objects.each do |k, v|
+        if objects.has_key?(k) && v.class_addr == objects[k].class_addr
+          objects.delete(k)
+        end
+      end
+    end
+
+    def keep_common!(other_heap)
+      objects.each do |k, v|
+        if !other_heap.objects.has_key?(k) || v.class_addr != other_heap.objects[k].class_addr
+          objects.delete(k)
+        end
+      end
+    end
+
+    def inspect
+      "Heap (objects: #{objects.size} types: #{types.size} refs: #{back_refs.size})"
+    end
+  end
+end
