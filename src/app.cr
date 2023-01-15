e545017859764b1584c5d6e0d28ab7f8e5c9a1d4
Stanisław Pitucha <stan.pitucha@envato.com>
2023-01-15 14:50:19 +1100

    Make the output target explicit for future testing

--- src/app.cr
+++ src/app.cr
@@ -21,42 +21,42 @@ def leaked_objects(heap_A : HeapLeak::Heap, heap_B : HeapLeak::Heap, heap_C : He
   new_heap
 end
 
-def print_dot_diagram(heap, visible_objects, sample_addresses)
+def print_dot_diagram(heap, visible_objects, sample_addresses, output)
   closest_common = visible_objects.select { |o| (heap.objects[o].seen_from || "").size > 1 }.min_by { |o| heap.objects[o].distance }
   max_distance = heap.objects[closest_common].distance
 
-  puts("# using a heap with #{heap.objects.size} objects")
-  puts("# a pool of #{visible_objects.size} interesting objects")
-  puts("# and #{sample_addresses.size} initial leaked objects")
-  puts("digraph leak {")
-  puts("rankdir=RL")
+  output.puts("# using a heap with #{heap.objects.size} objects")
+  output.puts("# a pool of #{visible_objects.size} interesting objects")
+  output.puts("# and #{sample_addresses.size} initial leaked objects")
+  output.puts("digraph leak {")
+  output.puts("rankdir=RL")
 
-  puts("# sample addresses")
-  puts("subgraph sources {")
-  puts("style=filled; color=lightgrey;")
+  output.puts("# sample addresses")
+  output.puts("subgraph sources {")
+  output.puts("style=filled; color=lightgrey;")
   sample_addresses.each { |sample_addr|
     current = heap.objects[sample_addr]
-    puts("\"0x#{sample_addr.to_s(16)}\" [color=blue, label=\"0x#{sample_addr.to_s(16)}\\n#{heap.type_of(sample_addr)}\\n#{current.file}:#{current.line}\"] ;")
+    output.puts("\"0x#{sample_addr.to_s(16)}\" [color=blue, label=\"0x#{sample_addr.to_s(16)}\\n#{heap.type_of(sample_addr)}\\n#{current.file}:#{current.line}\"] ;")
   }
-  puts("}")
+  output.puts("}")
 
-  puts("# other reachable entries")
+  output.puts("# other reachable entries")
   visible_objects.each { |current_addr|
     current = heap.objects[current_addr]
     next if sample_addresses.includes? current_addr
     next if current.distance > max_distance
-    puts("\"0x#{current_addr.to_s(16)}\" [label=\"0x#{current_addr.to_s(16)}\\n#{current.class_name}\\n#{current.file}:#{current.line}\"] ;")
+    output.puts("\"0x#{current_addr.to_s(16)}\" [label=\"0x#{current_addr.to_s(16)}\\n#{current.class_name}\\n#{current.file}:#{current.line}\"] ;")
   }
 
-  puts("# links")
+  output.puts("# links")
   visible_objects.each { |current_addr|
     next if heap.objects[current_addr].distance > max_distance
     heap.back_refs.fetch(current_addr, [] of HeapLeak::HEAP_ADDRESS).each {|ref|
       next if heap.objects[ref].distance > max_distance
       next unless visible_objects.includes? ref
-      puts("\"0x#{ref.to_s(16)}\" -> \"0x#{current_addr.to_s(16)}\" ;")
+      output.puts("\"0x#{ref.to_s(16)}\" -> \"0x#{current_addr.to_s(16)}\" ;")
     }
   }
 
-  puts("}")
+  output.puts("}")
 end
